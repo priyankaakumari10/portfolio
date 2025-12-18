5036a7323d8466815134ad1d5983868f3f570c56
Priyanka Kumari <priyankaakumari10@gmail.com>
2025-12-18 09:38:54 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
   images: {
     formats: ["image/avif", "image/webp"],
   },
-  // Optimize fonts
-  optimizeFonts: true,
 };
 
 export default nextConfig;
